e34f2ad20b1e96e1f0f9d0d40a8c4ccfe81ee609
Yogesh N <mindspring05@gmail.com>
2019-08-27 17:41:39 +0530

    Commit

--- GitHubTest/HelloGIT.py
+++ GitHubTest/HelloGIT.py
@@ -1,2 +1,3 @@
 print("Hello this is from my laptop to GitHub.com")
 print("This line was added from GitHub.com and pushed to GitHub installed laptop")
+print("This line was added from my laptop and pushed to GitHub.com")
